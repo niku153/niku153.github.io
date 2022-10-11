c87f9829c9075cee68f9008c47629433eb01a74e
niku153 <104482078+niku153@users.noreply.github.com>
2022-10-11 19:27:28 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", () => {
+  navUL.classList.toggle("show");
+});
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 4) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
